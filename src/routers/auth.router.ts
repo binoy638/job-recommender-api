4243ad90c00f71e9006cc433f6c751afec3aaaeb
binoy638 <binoy638@gmail.com>
2022-06-09 01:59:46 +0530

    fix: signout require login

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 
 import * as authController from '../controllers/auth.controller';
 import { getCurrentUser } from '../middlewares/currentUser.middleware';
+import { requireAuth } from '../middlewares/requireAuth.middlewares';
 import { loginValidator, registrationValidator } from '../middlewares/validator.middleware';
 
 const authRouter = Router();
 
 authRouter.post('/signup', registrationValidator, authController.signup);
 authRouter.post('/signin', loginValidator, authController.signin);
-authRouter.post('/signout', authController.signout);
+authRouter.post('/signout', getCurrentUser, requireAuth, authController.signout);
 authRouter.get('/current-user', getCurrentUser, authController.getCurrentUser);
 
 export default authRouter;
