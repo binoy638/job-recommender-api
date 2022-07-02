e95ba4f9df319f7fe734679d864beca1371978cf
binoy638 <binoy638@gmail.com>
2022-07-02 19:13:53 +0530

    fix: date strings thowing error during validation

--- src/validators/job.validator.ts
+++ src/validators/job.validator.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 
 import { JobMode, WorkHours } from '../models/jobs.schema';
 
+// eslint-disable-next-line consistent-return
+const dateSchema = z.preprocess(arg => {
+  if (typeof arg === 'string' || arg instanceof Date) return new Date(arg);
+}, z.date());
+
 export const jobPostSchema = z.object({
   jobTitle: z
     .string()
@@ -11,11 +16,15 @@ export const jobPostSchema = z.object({
   numberOfOpenings: z.number().min(1, { message: 'Number of openings should be at least 1' }),
   workHours: z.nativeEnum(WorkHours),
   category: z.string(),
-  applyBy: z.date(),
-  startDate: z.date(),
-  salaryMin: z.string().optional(),
-  salaryMax: z.string().optional(),
-  salaryNegotiable: z.boolean().optional(),
+  applyBy: dateSchema,
+  startDate: dateSchema,
+  salary: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+      negotiable: z.boolean(),
+    })
+    .optional(),
   description: z
     .string()
     .min(3, { message: 'Description should have at least 3 characters' })
